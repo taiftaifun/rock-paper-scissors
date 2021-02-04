9c79199bdf8cc04398997033817ade67f2ebe4f9
Mike <michalstefaniak@yahoo.com>
2021-02-04 21:09:09 +0100

    The game is mostly functioning, needs some polish

--- rps.js
+++ rps.js
@@ -1,19 +1,36 @@
-const possibilities = ["rock", "paper", "scissors"];
-const buttons = Array.from(document.querySelectorAll("button"));
-buttons.forEach(button => button.addEventListener("click", playerPlay));
+// set the initial scores at 0
+let playerScore = 0;
+let computerScore = 0;
 
+// define the dynamic DOM elements
+let playerChoiceDiv = document.querySelector("#player-choice");
+let computerChoiceDiv = document.querySelector("#computer-choice");
+let roundOutcomeDiv = document.querySelector("#round-outcome");
+let playerScoreDiv = document.querySelector("#player-score");
+let computerScoreDiv = document.querySelector("#computer-score");
+let scoreboardDiv = document.querySelector("#scoreboard");
+// let scoreboardDivInitial = document.querySelector("#scoreboard").innerHTML();
+
+// add events listeners to the buttons pressed by the player
+const buttons = Array.from(document.querySelectorAll("button"));
+buttons.forEach(button => button.addEventListener("click", playRound));
+
+// define the random computer choice
+const possibilities = ["rock", "paper", "scissors"];
 function computerPlay() {
     return possibilities[Math.round(Math.random()*2)];
 }
 
+// define the generation of player's choice upon click
 function playerPlay(e) {
-    return(e.target.id);
+    let choice = e.target.id; 
+    return(choice);
 }
 
-/*
-function playRound() {
-    let playerSelection=playerPlay();
-    let computerSelection=computerPlay();    
+// define the function playing out a single round
+function playRound(e) {
+    let playerSelection=playerPlay(e);
+    let computerSelection=computerPlay();
     console.log(`Player selected ${playerSelection}`);
     console.log(`Computer selected ${computerSelection}`)
     let winner="";    
@@ -51,31 +68,35 @@ function playRound() {
     } else if(winner=="tie") {
         outcome=`It's a tie! ${computerSelection} does not beat ${playerSelection}`
     }
-    console.log(outcome);
-    return outcome;
+    
+    playerChoiceDiv.textContent = `Player selected ${playerSelection}`
+    computerChoiceDiv.textContent = `Computer selected ${computerSelection}`
+    roundOutcomeDiv.textContent = `${outcome}`;
+    updateScore(outcome);
+    
+    // if(playerScore==5) {
+    //     scoreboardDiv.textContent = "You were the first to get the 5 points, you win!";
+    //     await new Promise(r => setTimeout(r, 2));
+    //     computerScore = 0;
+    //     playerScore = 0;
+    // } else if(computerScore==5) {
+    //     scoreboardDiv.textContent = "The computer was the first to get the 5 points, you lose!";
+    //     await new Promise(r => setTimeout(r, 2));
+    //     scoreboardDiv.innerHTML = scoreboardDivInitial;
+    //     computerScore = 0;
+    //     playerScore = 0;
+    // }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let singleOutcome = "";
+// define the score update mechanism
+function updateScore(outcome) {
     let finalOutcome = "";
-    while((playerScore<5) && (computerScore<5)) {
-        singleOutcome = playRound();
+        let singleOutcome = outcome;
         if(singleOutcome.includes("You won")) {
             playerScore++;
         } else if(singleOutcome.includes("You lost")) {
             computerScore++;
         }
-        console.log(`Player score: ${playerScore}`);
-        console.log(`Computer score: ${computerScore}`);
-        console.log("--------------------------------")
-    }
-    if(playerScore==5) {
-        finalOutcome="You were the first to get 5 points. You win!";
-    } else if(computerScore==5) {
-        finalOutcome="Computer was the first to get 5 points. You lose!";
-    }
-    return finalOutcome;
+        playerScoreDiv.textContent = playerScore;
+        computerScoreDiv.textContent = computerScore;
 }
-*/
